621084be063a42997b321b15c561125e3af269a1
Israel Malagutti <israelmalagutti@gmail.com>
2023-08-25 15:55:13 -0300

    feat: add glowing effect to cart button

--- src/components/Layout/Header/HeaderActions.jsx
+++ src/components/Layout/Header/HeaderActions.jsx
@@ -4,8 +4,19 @@ import { ShoppingCart } from "lucide-react";
 export function HeaderActions({ shopButton, cartButton }) {
   const CartButton = () =>
     cartButton ? (
-      <button className="py-3 px-4 bg-green-600 hover:bg-green-700 dark:bg-white dark:hover:bg-zinc-200 text-white dark:text-black font-semibold rounded-md">
+      <button
+        aria-description="Button that opens the cart"
+        className="relative py-3 px-4 bg-green-500 hover:bg-green-400 shadow-lg hover:shadow-[0_0_16px_rgba(0,0,0,0.25)] hover:shadow-green-500 dark:bg-green-600 dark:hover:bg-green-500 text-white dark:text-black font-semibold rounded-md transition-colors"
+      >
         <ShoppingCart size={24} />
+
+        <div
+          aria-details="number of products in the cart"
+          className="w-5 h-5 absolute flex items-center justify-center top-[15%] right-[15%] bg-red-500 text-xs text-white rounded-full"
+        >
+          12
+          {/*{meals.length}*/}
+        </div>
       </button>
     ) : (
       <React.Fragment></React.Fragment>
