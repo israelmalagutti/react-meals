7b0d89a84ddceec8e8e1c12a80a474535bcbdfb1
Israel Malagutti <israelmalagutti@gmail.com>
2023-09-14 18:09:43 -0300

    chore: change width and max height of modals

--- src/components/Modals/ModalOverlay/ModalRoot.jsx
+++ src/components/Modals/ModalOverlay/ModalRoot.jsx
@@ -4,7 +4,7 @@ export function ModalRoot({ children, className = "" }) {
   return (
     <div
       className={cn(
-        "w-3/4 h-3/4 fixed z-50 inset-0 m-auto overflow-hidden",
+        "w-1/2 max-h-[75%] fixed z-50 inset-0 m-auto overflow-hidden",
         className && className
       )}
     >
